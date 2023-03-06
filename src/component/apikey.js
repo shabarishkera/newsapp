99114e2d770a385f61ac166b02e2b22337968df4
shabarish <shabarishkera@gmail.com>
2023-03-06 19:00:20 +0530

    adding pinner to archive

--- src/component/apikey.js
+++ src/component/apikey.js
@@ -1,2 +1,9 @@
-
+let links={
+ tech:"https://newsapi.org/v2/everything?q=tech&apiKey=44e64c5f16f24e5db2dbb6ec66047e60",
+ health:"https://newsapi.org/v2/everything?q=health&apiKey=44e64c5f16f24e5db2dbb6ec66047e60",
+ general:"https://newsapi.org/v2/everything?q=india&apiKey=44e64c5f16f24e5db2dbb6ec66047e60",
+ buisness:"https://newsapi.org/v2/everything?q=buisness&apiKey=44e64c5f16f24e5db2dbb6ec66047e60",
+ sports:"https://newsapi.org/v2/everything?q=sports&apiKey=44e64c5f16f24e5db2dbb6ec66047e60",
+ entatainment:"https://newsapi.org/v2/everything?q=entertainment&apiKey=44e64c5f16f24e5db2dbb6ec66047e60",
+}
 export default links;
